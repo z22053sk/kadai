d50e258bd115a23fb9238883e1c7463434384d02
z22053sk <107286961+z22053sk@users.noreply.github.com>
2022-06-15 22:34:09 +0900

    Update 1-1_fizz_buzz.py

--- 1-1_fizz_buzz.py
+++ 1-1_fizz_buzz.py
@@ -1,9 +1,9 @@
 #課題1-1: fizz_buzz.py
 def fb(i):
 
-    a = [[i%15,"FizzBuzz"],[i%5,"Buzz"],[i%3,"Fizz"],[0,""]] #余りを計算し、それぞれリストに代入する（15,5,3で割れる数値、配列4つ目は強制的に0で値無し） 
-    a.sort(reverse=False,key=lambda x:x[0])                  #最小値をソートする (「0」であれば割り切れる。割り切れていない場合は値無し"空"）
-    ret = a[:][0][1]                                         #最小値を抽出する、文字列のみを取り出す → ret = ret[1]  #文字のみを抽出する　[[0],"※"] 
+    a = [[i%15,"FizzBuzz"],[i%5,"Buzz"],[i%3,"Fizz"],[0,""]] 
+    a.sort(reverse=False,key=lambda x:x[0])                 
+    ret = a[:][0][1]
     return ret
 
 i = 1
