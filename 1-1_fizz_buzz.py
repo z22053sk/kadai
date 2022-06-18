96f1eeeeae5c028b4d40158f49876106da921526
z22053sk <107286961+z22053sk@users.noreply.github.com>
2022-06-18 10:14:45 +0900

    Update 1-1_fizz_buzz.py

--- 1-1_fizz_buzz.py
+++ 1-1_fizz_buzz.py
@@ -1,8 +1,8 @@
 #課題1-1: fizz_buzz.py
 def fb(i):
 
-    a = [[i%15,"FizzBuzz"],[i%5,"Buzz"],[i%3,"Fizz"],[0,""]] 
-    a.sort(reverse=False,key=lambda x:x[0])                 
+    a = [[i%15,"FizzBuzz"],[i%5,"Buzz"],[i%3,"Fizz"],[0,""]]
+    a.sort(reverse = False , key=lambda x:x[0])                
     ret = a[:][0][1]
     return ret
 
